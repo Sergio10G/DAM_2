1d64e84f7db3b8869dc84ef0d20145593784f738
Sergio10G <sejobmx@hotmail.es>
2021-11-18 18:39:49 +0100

    18/11/2021 - casa4

--- Programación de Servicios y Procesos/Proyectos/Hito2/src/com/sdiezg/Interseccion/Coche.java
+++ Programación de Servicios y Procesos/Proyectos/Hito2/src/com/sdiezg/Interseccion/Coche.java
@@ -2,6 +2,7 @@ package com.sdiezg.Interseccion;
 
 public class Coche {
 	
-	// Utilizo la clase coche como placeholder, para guar
+	// Utilizo la clase coche como placeholder, para guardar coches en la lista.
+	// No voy a necesitar guardar ningún tipo de datos acerca de los coches.
 	
 }
