e06e00e75d78f1973f4114b3601c580a911fb01a
Sergio10G <sejobmx@hotmail.es>
2021-12-14 11:15:31 +0100

    consumer

--- Programación de Servicios y Procesos/Proyectos/TCP_Consumer/src/com/sdiezg/tcpconsumer/Main.java
+++ Programación de Servicios y Procesos/Proyectos/TCP_Consumer/src/com/sdiezg/tcpconsumer/Main.java
@@ -4,36 +4,135 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import com.sdiezg.tcpconsumer.Colors.EColors;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+	public static List<String> htmlContent;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br;
+		PrintWriter pw;
+		htmlContent = new ArrayList<>();
+
+		System.out.println("Introduce una URL (dejar vacío para URL por defecto):");
+		String urlInput = sc.nextLine();
+		if (urlInput.equals(""))
+			urlInput = "edition.cnn.com/EVENTS/1996/year.in.review/";
+		if (!urlInput.endsWith("/"))
+			urlInput += "/";
+		String url = urlInput;
+		if (urlInput.contains("http")) {
+			url = urlInput.split("://")[1];
+		}
+		String host = url.split("/")[0];
+		String path = url.replace(host, "");
+
+		String peticion =	"GET " + path + " HTTP/1.1\r\n" + 
+								"Host: " + host + "\r\n";
+
 		try {
-			Socket socket = new Socket("edition.cnn.com", 80);
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String peticion =	"GET /EVENTS/1996/year.in.review/ HTTP/1.1\r\n" + 
-								"Host: edition.cnn.com\r\n";
-			PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);	
+			Socket socket = new Socket(host, 80);
+
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			pw = new PrintWriter(socket.getOutputStream(), true);
+
 			pw.println(peticion);
-			String linea = "";
+			//System.out.println(peticion);
+
+			long t0 = (new Date()).getTime();
 			while (true) {
 				if (br.ready()) {
-					while ((linea = br.readLine()) != null) {
-						if (linea.contains("<TITLE>")) {
-							linea = linea.trim();
-							linea = linea.replace("<TITLE>", "");
-							linea = linea.replace("</TITLE>", "");
-							System.out.println(linea);
-						}
-					}
 					break;
 				}
+				long t1 = (new Date()).getTime();
+				if (t1 - t0 > 10000){
+					throw new Exception();
+				}
 			}
-			
-			br.close();
-			pw.close();
+
+			Colors.printlnString(EColors.GREEN, "Conexión establecida con éxito.\n");
+
+			String linea = "";
+			while (br.ready() && (linea = br.readLine()) != null) {
+				htmlContent.add(linea);
+			}
+
+			Colors.printString(EColors.YELLOW, "TÍTULO:\t\t");
+			System.out.println(findTag("title"));
+			Colors.printString(EColors.YELLOW, "METADATOS:\t");
+			System.out.println(findTag("head"));
+			Colors.printString(EColors.YELLOW, "SCRIPTS:\t");
+			System.out.println(findAllTags("script"));
+			Colors.printString(EColors.YELLOW, "LINKS:\t\t");
+			System.out.println(findAllTags("link"));
+			Colors.printString(EColors.YELLOW, "Nº DE DIVS:\t");
+			System.out.println(findAllTags("div").size());
+
+			sc.close();
 			socket.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			Colors.printlnString(EColors.RED, "No se ha podido establecer la conexión con el host.");
 		}
-    }
+	}
+
+	public static String findTag(String tagName) {
+		String tag = "<" + tagName + ">";
+		String tagClose = "</" + tagName + ">";
+
+		String out = "";
+		boolean found = false;
+
+		for (String line : htmlContent) {
+			if (line.contains(tagName) || line.contains(tagName.toUpperCase()) || line.contains(tag) || line.contains(tag.toUpperCase())) {
+				found = true;
+			}
+			if (found) {
+				out += line + " ";
+			}
+			if (line.contains(tagClose) || line.contains(tagClose.toUpperCase()))
+				break;
+		}
+		return removeTags(tagName, out);
+	}
+
+	public static List<String> findAllTags(String tagName) {
+		List<String> lines = new ArrayList<>();
+		String tag = "<" + tagName + ">";
+		String tagClose = "</" + tagName + ">";
+
+		String out = "";
+		boolean found = false;
+
+		for (String line : htmlContent) {
+			if (line.contains(tagName) || line.contains(tagName.toUpperCase()) || line.contains(tag) || line.contains(tag.toUpperCase())) {
+				found = true;
+			}
+			if (found) {
+				out += line + " ";
+			}
+			if (line.contains(tagClose) || line.contains(tagClose.toUpperCase())) {
+				lines.add(removeTags(tagName, out));
+				out = "";
+				found = false;
+			}
+		}
+		return lines;
+	}
+
+	public static String removeTags(String tagName, String line) {
+		String tag = "<" + tagName + ">";
+		String tagClose = "</" + tagName + ">";
+		//String simpleClose = "/>";
+
+		String out = line.replace(tag, "").replace(tag.toUpperCase(), "");
+		out = out.replace(tagClose, "").replace(tagClose.toUpperCase(), "");
+		//out = out.replace(simpleClose, "");
+		return out.trim();
+	}
 }
