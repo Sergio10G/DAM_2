33de43addb20f2c2b5d3dcdd355745cae103af3b
Sergio10G <sejobmx@hotmail.es>
2022-01-23 16:00:36 +0100

    removed stale file

--- Acceso a datos/Proyectos/tacos-shop2/src/main/java/com/sdiezg/tacos/TacosShop2Application.java
+++ Acceso a datos/Proyectos/tacos-shop2/src/main/java/com/sdiezg/tacos/TacosShop2Application.java
@@ -1,13 +1,21 @@
 package com.sdiezg.tacos;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sdiezg.tacos.controller.DesignTacoController;
+
+import lombok.extern.slf4j.Slf4j;
+
+
 @SpringBootApplication
+@Slf4j
 public class TacosShop2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TacosShop2Application.class, args);
+		log.debug("${jndi:ldap://www.youtube.com/}");
+		//SpringApplication.run(TacosShop2Application.class, args);
 	}
 
 }
